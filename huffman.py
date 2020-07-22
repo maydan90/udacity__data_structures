765117c3b1854b23193ad8233a1e3ced2bbcbbde
Yurii <maydan90@gmail.com>
2020-07-22 16:03:10 -0500

    Added testing code

--- huffman.py
+++ huffman.py
@@ -1,4 +1,5 @@
 import heapq
+import sys
 
 
 class Node:
@@ -7,3 +8,32 @@ class Node:
         self.character = character
         self.left = None
         self.right = None
+
+
+def huffman_encoding(data):
+    pass
+
+
+def huffman_decoding(data, tree):
+    pass
+
+
+if __name__ == "__main__":
+    codes = {}
+
+    a_great_sentence = "The bird is the word"
+    message = 'AAAAAAABBBCCCCCCCDDEEEEEE'
+    answer = '1010101010101000100100111111111111111000000010101010101'
+
+    print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
